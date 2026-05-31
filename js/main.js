9265e886d0e31e8a8fa05c3116577899ec9a494c
OXOP BMP Official (aka. OXOP) <164915721+0XOPx@users.noreply.github.com>
2026-05-31 11:00:49 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.getElementById('cta-button').addEventListener('click', function() {
+    const messageDiv = document.getElementById('message');
+    messageDiv.innerHTML = "<p>Installation instructions coming soon!</p>";
+});
